a6dcc538640cab62861d6bab5feb28c908333c66
Steffen Roecker <sroecker@redhat.com>
2024-11-15 17:32:33 +0100

    Update websearch to use a larger model

--- notebooks/websearch.py
+++ notebooks/websearch.py
@@ -4,11 +4,13 @@ from phi.tools.duckduckgo import DuckDuckGo
 
 web_agent = Agent(
     name="Web Agent",
-    model=OllamaTools(id="llama3.2:3b-instruct-q8_0"),
+    #model=OllamaTools(id="llama3.2:3b-instruct-q8_0"),
     #model=OllamaTools(id="qwen2.5:3b-instruct-q8_0"),
     #model=OllamaTools(id="qwen2.5"),
+    model=OllamaTools(id="hermes3:8b-llama3.1-q8_0"),
     tools=[DuckDuckGo()],
     show_tool_calls=True,
     markdown=True,
 )
 web_agent.print_response("What are the latest news from Darmstadt?", stream=True)
+#web_agent.cli_app(stream=False)
